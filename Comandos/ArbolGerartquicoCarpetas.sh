1ff821fabff80015b182b716b886911c15e30c28
adrigo <adrian.gd@live.com>
2020-06-25 17:33:05 +0200

    Now you can provide or not a folder, if you dont it will show de folder you are located at

--- Comandos/ArbolGerartquicoCarpetas.sh
+++ Comandos/ArbolGerartquicoCarpetas.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Script that shows a hierarchical tree of folders."
+echo "If folder is not provided it will show of folder you are located atd."
 echo -n "Indica la carpeta(Ruta absoluta): "
 read carpeta
-
-ls -R $carpeta | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+if [ -z "$carpeta" ]
+then
+  ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+else
+  ls -R $carpeta | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+fi
